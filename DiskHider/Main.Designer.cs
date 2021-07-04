43e9ad6c25a7cbfdc17f67165203963130fb69e0
Kotikgames <a@a.a>
2021-07-04 16:11:06 +0300

    Обнаружение дисков

--- DiskHider/Main.Designer.cs
+++ DiskHider/Main.Designer.cs
@@ -29,13 +29,86 @@ namespace DiskHider
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.drivesList = new System.Windows.Forms.CheckedListBox();
+            this.commandLink3 = new DiskHider.CommandLink();
+            this.commandLink2 = new DiskHider.CommandLink();
+            this.commandLink1 = new DiskHider.CommandLink();
+            this.hideSelectedDrivesButton = new DiskHider.CommandLink();
+            this.SuspendLayout();
+            // 
+            // drivesList
+            // 
+            this.drivesList.FormattingEnabled = true;
+            this.drivesList.Location = new System.Drawing.Point(-1, 3);
+            this.drivesList.MultiColumn = true;
+            this.drivesList.Name = "drivesList";
+            this.drivesList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.drivesList.Size = new System.Drawing.Size(245, 199);
+            this.drivesList.TabIndex = 0;
+            // 
+            // commandLink3
+            // 
+            this.commandLink3.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.commandLink3.Location = new System.Drawing.Point(250, 142);
+            this.commandLink3.Name = "commandLink3";
+            this.commandLink3.Size = new System.Drawing.Size(240, 48);
+            this.commandLink3.TabIndex = 5;
+            this.commandLink3.Text = "Показать все диски";
+            this.commandLink3.UseVisualStyleBackColor = true;
+            // 
+            // commandLink2
+            // 
+            this.commandLink2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.commandLink2.Location = new System.Drawing.Point(250, 98);
+            this.commandLink2.Name = "commandLink2";
+            this.commandLink2.Size = new System.Drawing.Size(240, 38);
+            this.commandLink2.TabIndex = 4;
+            this.commandLink2.Text = "Скрыть все диски";
+            this.commandLink2.UseVisualStyleBackColor = true;
+            // 
+            // commandLink1
+            // 
+            this.commandLink1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.commandLink1.Location = new System.Drawing.Point(250, 48);
+            this.commandLink1.Name = "commandLink1";
+            this.commandLink1.Size = new System.Drawing.Size(240, 44);
+            this.commandLink1.TabIndex = 3;
+            this.commandLink1.Text = "Показать выбранные диски";
+            this.commandLink1.UseVisualStyleBackColor = true;
+            // 
+            // hideSelectedDrivesButton
+            // 
+            this.hideSelectedDrivesButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.hideSelectedDrivesButton.Location = new System.Drawing.Point(250, 3);
+            this.hideSelectedDrivesButton.Name = "hideSelectedDrivesButton";
+            this.hideSelectedDrivesButton.Size = new System.Drawing.Size(240, 41);
+            this.hideSelectedDrivesButton.TabIndex = 2;
+            this.hideSelectedDrivesButton.Text = "Скрыть выбранные диски";
+            this.hideSelectedDrivesButton.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(495, 202);
+            this.Controls.Add(this.commandLink3);
+            this.Controls.Add(this.commandLink2);
+            this.Controls.Add(this.commandLink1);
+            this.Controls.Add(this.hideSelectedDrivesButton);
+            this.Controls.Add(this.drivesList);
+            this.Name = "Main";
+            this.Text = "DiskHider";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox drivesList;
+        private DiskHider.CommandLink hideSelectedDrivesButton;
+        private DiskHider.CommandLink commandLink1;
+        private DiskHider.CommandLink commandLink2;
+        private DiskHider.CommandLink commandLink3;
     }
 }
 
--- DiskHider/Main.cs
+++ DiskHider/Main.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
 
 namespace DiskHider
 {
     public partial class Main : Form
     {
+        [Flags]
+        public enum DriveIds
+        {
+            Empty = 0,
+            A = 1,
+            B = 2,
+            C = 4,
+            D = 8,
+            E = 16,
+            F = 32,
+            G = 64,
+            H = 128,
+            I = 256,
+            J = 512,
+            K = 1024,
+            L = 2048,
+            M = 4096,
+            N = 8192,
+            O = 16384,
+            P = 32768,
+            Q = 65536,
+            R = 131072,
+            S = 262144,
+            T = 524288,
+            U = 1048576,
+            V = 2097152,
+            W = 4194304,
+            X = 8388608,
+            Y = 16777216,
+            Z = 33554432,
+        }
         public Main()
         {
+            RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer");
+            object value = key.GetValue("NoDrives");
+            DriveIds hiddenDrives = value == null ? DriveIds.Empty : (DriveIds)value;
             InitializeComponent();
+            foreach(DriveInfo drive in DriveInfo.GetDrives())
+            {
+                string letter = drive.Name.Remove(1);
+                bool has = hiddenDrives.HasFlag((DriveIds)Enum.Parse(hiddenDrives.GetType(), letter));
+                drivesList.Items.Add(letter, has);
+            }
+        }
+    }
+    public class CommandLink : Button
+    {
+        const int BS_COMMANDLINK = 0x0000000E;
+
+        public CommandLink()
+        {
+            FlatStyle = FlatStyle.System;
+        }
+
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cParams = base.CreateParams;
+                cParams.Style |= BS_COMMANDLINK;
+                return cParams;
+            }
         }
     }
 }
